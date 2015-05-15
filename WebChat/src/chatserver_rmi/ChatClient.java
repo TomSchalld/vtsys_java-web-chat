06db98d999d6f8d0b31edae1a61bf5c7f8587a5a
Thomas Schalldach <register@thomas-schalldach.de>
2015-05-15 21:11:45 +0200

    upload

---
+++ WebChat/src/chatserver_rmi/ChatClient.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chatserver_rmi;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.rmi.AccessException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Thomas Schalldach & Fabian Brammer Copyright 2015
+ */
+public class ChatClient implements Serializable {
+	ChatProxy cp;
+	ClientProxy client;
+	String username;
+	ChatServer server;
+	boolean newMessage;
+	boolean isWebUser;
+	String message;
+
+	public boolean hasNewMessage() {
+		return newMessage;
+	}
+
+	public void setNewMessage(boolean newMessage) {
+		this.newMessage = newMessage;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public ChatClient() throws RemoteException, MalformedURLException,
+			NotBoundException {
+		super();
+		this.username = IO.readString("Username: ");
+		String serverAddress = IO.readString("IP-Adresse des Servers: ");
+		this.server = (ChatServer) Naming.lookup("rmi://" + serverAddress
+				+ ":1099/ChatServer");
+		this.client = new ClientProxyImpl(this);
+		System.out.println(this.username + " wird eingeloggt ...");
+		this.cp = server.subscribeUser(this.username, client);
+		this.isWebUser = false;
+		if (cp != null) {
+			System.out.println("... done.");
+		}
+	}
+
+	public ChatClient(String username) throws RemoteException,
+			MalformedURLException, NotBoundException {
+		super();
+		this.username = username;
+		this.server = (ChatServer) Naming
+				.lookup("rmi://localhost:1099/ChatServer");
+		this.client = new ClientProxyImpl(this);
+		System.out.println(this.username + " wird eingeloggt ...");
+		this.cp = server.subscribeUser(this.username, client);
+		this.newMessage = false;
+		this.message = null;
+		this.isWebUser = true;
+		if (cp != null) {
+			System.out.println("... done.");
+		}
+	}
+
+	public void logout() throws RemoteException {
+		if (this.server.unsubscribeUser(this.username)) {
+			System.out.println(this.username + " erfolgreich ausgeloggt.");
+			if(!this.isWebUser){
+				System.exit(0);
+			}
+		} else {
+			System.out.println(this.username + " fehler beim ausloggen.");
+		}
+	}
+
+	public void recieveMessage(String username, String message)
+			throws RemoteException {
+		// TODO Auto-generated method stub
+		if (!this.isWebUser) {
+			this.postMessage(username, message);
+		} else {
+			this.postMessageToWeb(username, message);
+		}
+	}
+
+	public void postMessage(String username, String message) {
+		System.out.println(username + " schreibt:  " + message);
+	}
+
+	public void postMessageToWeb(String username, String message) {
+
+		this.message = (username + " schreibt: " + message);
+		this.setNewMessage(true);
+
+	}
+
+	public void sendMessage(String message) throws RemoteException {
+		this.cp.sendMessage(message);
+	}
+
+	public ChatProxy getCp() {
+		return cp;
+	}
+
+	public ClientProxy getClient() {
+		return client;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public ChatServer getServer() {
+		return server;
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		try {
+			ChatClient client = new ChatClient();
+
+			while (true) {
+				String massage = IO.readString("@" + client.getUsername()
+						+ ": ");
+				if (massage.equals("exit")) {
+					client.logout();
+					break;
+				}
+				client.sendMessage(massage);
+			}
+		} catch (AccessException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ WebChat/src/web/HalloWelt.java
@@ -0,0 +1,82 @@
+package web;
+
+import java.io.*;
+import java.rmi.NotBoundException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.apache.catalina.SessionEvent;
+
+import chatserver_rmi.ChatClient;
+
+public class HalloWelt extends HttpServlet {
+	static List<ChatClient> clients = new LinkedList<ChatClient>();
+	static HttpSession session;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/plain;charset=UTF-8");
+		// Allocate a output writer to write the response message into the
+		// network socket
+		
+		PrintWriter out = response.getWriter();
+		System.out.println(request.getParameter("username"));
+		if (request.getParameter("method").equals("subscribe")) {
+			try {
+				clients.add(new ChatClient(request.getParameter("username")));
+				session = request.getSession();
+				
+			} catch (NotBoundException e) {
+				e.printStackTrace();
+			}
+		} else if (request.getParameter("method").equals("unsubscribe")) {
+			ChatClient tmp = this.search(request.getParameter("username"));
+			if (tmp != null) {
+				tmp.logout();
+				clients.remove(tmp);
+			}
+		} else if (request.getParameter("method").equals("SEND")) {
+			ChatClient tmp = this.search(request.getParameter("username"));
+			if(tmp!=null){
+				tmp.sendMessage(request.getParameter("message"));
+				out.println(request.getParameter("message"));
+			}
+		} else if (request.getParameter("method").equals("RECIEVE")) {
+			ChatClient tmp = this.search(request.getParameter("username"));
+			while(!tmp.hasNewMessage()){
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(tmp.hasNewMessage()){
+				out.println(tmp.getMessage());
+				tmp.setNewMessage(false);
+			}
+			
+		}
+
+		// Write the response message, in an HTML page
+		try {
+
+		} finally {
+			out.close(); // Always close the output writer
+		}
+	}
+
+	public ChatClient search(String username) {
+		for (ChatClient cc : clients) {
+			if (cc.getUsername().equals(username)) {
+				return cc;
+			}
+		}
+		return null;
+	}
+
+}
